eb1abb0e9b592d3009ba010b883c943a0216cdf3
yxxnghwan <younghwan960@gmail.com>
2023-12-04 01:36:12 +0900

    project init

---
+++ src/main/kotlin/com/example/springsecuritytutorial/SpringSecurityTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springsecuritytutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springsecuritytutorial/SpringSecurityTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springsecuritytutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
